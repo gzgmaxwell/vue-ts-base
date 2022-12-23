633db130a8543d319e064f0ab31dfa717105424b
gouzhigang <674244021@qq.com>
2022-12-23 17:07:52 +0800

    feat(新增vant组建包模块): vant 组件语言包配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { watch } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { Locale } from 'vant'
+import enUS from 'vant/es/locale/lang/en-US'
+import zhCN from 'vant/es/locale/lang/zh-CN'
+
 import { useAppStore } from './stores'
 
 const { locale: i18NLocale } = useI18n({ useScope: 'global' })
@@ -10,6 +14,11 @@ watch(
   () => appStore.locale,
   newLocale => {
     i18NLocale.value = newLocale
+    if (newLocale === 'enUS') {
+      Locale.use('en-US', enUS)
+    } else {
+      Locale.use('en-CN', zhCN)
+    }
   }
 )
 </script>
