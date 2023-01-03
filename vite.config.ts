97856f9c00075fd53e7ae38b80eebc3d7af81a68
Mathon <cnmathon@gmail.com>
2023-01-03 16:10:04 +0800

    fix(vite.config.ts): 修复传入的 visualizer plugin 类型错误的问题

--- vite.config.ts
+++ vite.config.ts
@@ -75,7 +75,7 @@ if (IS_PRODUCTION) {
       open: true, // 在默认用户代理中打开生成的文件
       gzipSize: true, // 从源代码中收集 gzip 大小并将其显示在图表中
       brotliSize: true // 从源代码中收集 brotli 大小并将其显示在图表中
-    })
+    }) as PluginOption
   )
 }
 
